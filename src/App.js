54c2e793ad1c6386a1ca3a1ad0ccc5ba5f2e8379
Ryan Park <ryan.developer99@gmail.com>
2021-06-29 17:12:52 +0900

    add: questions
     add questions do questions file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import Questions from "./Questions";
+import React, { useState } from "react";
+import { questions } from "./questions";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  console.log(questions[number]);
+
   return (
-    <div>
-      <h1>hello world</h1>
-      <Questions />
-    </div>
+    <>
+      <div className="wrapper">
+        <section className="container">
+          <h1>Basic CS QUIZ</h1>
+          <article className="question-section">
+            <h3>
+              {number + 1}.{questions[number].questionText}
+            </h3>
+          </article>
+          <article className="answer-section">
+            <h1>Answer section</h1>
+          </article>
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/Questions.js
+++ src/Questions.js
@@ -1,11 +1,22 @@
-import React from "react";
-
-const Questions = () => {
-  return (
-    <div>
-      <h1>Questions</h1>
-    </div>
-  );
-};
-
-export default Questions;
+export const questions = [
+  {
+    id: 1,
+    questionText: "What is the capital of USA?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Paris", isCorrect: true },
+      { answerText: "Washington D.C", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    questionText: "What is the difference between ",
+    answerOptions: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "4", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+    ],
+  },
+];
